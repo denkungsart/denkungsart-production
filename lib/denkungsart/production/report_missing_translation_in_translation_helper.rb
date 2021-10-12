4d576c8e113ba8abca78d560986be0a6e7e36429
Andreas Venturini <andreas.venturini@gmail.com>
2021-10-12 19:10:07 +0200

    Use double splat to pass keyword args
    ref https://www.ruby-lang.org/en/news/2019/12/12/separation-of-positional-and-keyword-arguments-in-ruby-3-0/

--- lib/denkungsart/production/report_missing_translation_in_translation_helper.rb
+++ lib/denkungsart/production/report_missing_translation_in_translation_helper.rb
@@ -1,8 +1,8 @@
 module ReportMissingTranslationInTranslationHelper
   def t(key, options = {})
-    translate(key, options.reverse_merge(raise: true))
+    translate(key, **options.reverse_merge(raise: true))
   rescue I18n::MissingTranslationData => e
     I18n.exception_handler.report_exception(e, e.locale, e.key, e.options)
-    translate(key, options)
+    translate(key, **options)
   end
 end
